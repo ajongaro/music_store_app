6b36f916bb24ec3c5ac3e333350e031cd331022e
Anthony Ongaro <43623494+ajongaro@users.noreply.github.com>
2022-12-04 16:01:49 -0700

    fix: Update schema, remove unused instrument category from schema, add faker seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,24 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+require 'faker'
+
+# generate 10 stores
+10.times do
+  Store.create!(
+    name: Faker::Company.unique.name + " Music",
+    max_inventory: rand(99..999),
+    online_only: Faker::Boolean.boolean(true_ratio: 0.3),
+    customer_rating: rand(1..5),
+  )
+end
+
+100.times do
+  Instrument.create!(
+    name: "#{Faker::Commerce.brand} #{Faker::Music.instrument}",
+    used: Faker::Boolean.boolean(true_ratio: 0.4),
+    needs_repair: Faker::Boolean.boolean(true_ratio: 0.2),
+    price: [rand(59..999), rand(59..999), rand(999..1999), rand(4999..9999)].sample,
+    store_id: rand(1..10)
+  )
+end
