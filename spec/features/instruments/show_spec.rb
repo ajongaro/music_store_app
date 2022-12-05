c052ec11e0e0a9d6d7efcd80e9693f20dea31eb0
Anthony Ongaro <43623494+ajongaro@users.noreply.github.com>
2022-12-05 16:30:40 -0700

    test: Update instruments show_spec to use let blocks

--- spec/features/instruments/show_spec.rb
+++ spec/features/instruments/show_spec.rb
@@ -3,16 +3,16 @@
 require 'rails_helper'
 
 RSpec.describe 'the instrument show page', type: :feature do
+  let!(:store) { Store.create!(name: "Guitar Center", online_only: false, max_inventory: 999, customer_rating: 5) }
+  let!(:store_2) { Store.create!(name: "Dave's Music Shop", online_only: false, max_inventory: 100, customer_rating: 4) }
+  let!(:store_3) { Store.create!(name: "Pete's Grand Piano Emporium", online_only: true, max_inventory: 20, customer_rating: 2) }
+  let!(:guitar) { store.instruments.create!(name: "Gibson Dot Guitar", used: true, needs_repair: false, price: 1399) }
+  let!(:piano) { store.instruments.create!(name: "Steinway Grand Piano", used: false, needs_repair: false, price: 61299) }
+  let!(:banjo) { store_2.instruments.create!(name: "Fender Banjo", used: false, needs_repair: false, price: 61299) }
+
   describe 'as a visitor' do
     describe "when I visit '/instruments/:id'" do
       it 'I see that instrument name and its attributes' do
-        store = Store.create!(name: "Guitar Center", online_only: false, max_inventory: 999, customer_rating: 5)
-        store_2 = Store.create!(name: "Dave's Music Shop", online_only: false, max_inventory: 100, customer_rating: 4)
-        
-        guitar = store.instruments.create!(name: "Gibson Dot Guitar", used: true, needs_repair: false, price: 1399)
-        piano = store.instruments.create!(name: "Steinway Grand Piano", used: false, needs_repair: false, price: 61299)
-        banjo = store_2.instruments.create!(name: "Fender Banjo", used: false, needs_repair: false, price: 61299)
-
         visit "/instruments/#{piano.id}"
 
         expect(page).to have_content("Steinway Grand Piano")
